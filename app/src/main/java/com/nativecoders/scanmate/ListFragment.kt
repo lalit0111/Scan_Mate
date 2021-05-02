659b9c8a2bc9bdb001dde70d671937352e621749
Lalit Tilwani <tilwanil818@gmail.com>
2021-05-02 17:23:25 +0530

    camera setup.

--- app/src/main/java/com/nativecoders/scanmate/ListFragment.kt
+++ app/src/main/java/com/nativecoders/scanmate/ListFragment.kt
@@ -4,6 +4,7 @@ import `in`.balakrishnan.easycam.CameraBundleBuilder
 import `in`.balakrishnan.easycam.CameraControllerActivity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -26,40 +27,24 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
         intent.putExtra(
             "inputData", CameraBundleBuilder()
-                .setFullscreenMode(false)
-                .setDoneButtonString("Add")
+                .setFullscreenMode(true)
                 .setSinglePhotoMode(false)
-                .setMax_photo(3)
+                .setEnableDone(false)
+                .setCaptureButtonDrawable(R.drawable.group_1)
+                .setMax_photo(50)
                 .setManualFocus(true)
                 .setBucketName(javaClass.name)
                 .setPreviewEnableCount(true)
                 .setPreviewIconVisiblity(true)
                 .setPreviewPageRedirection(true)
-                .setEnableDone(false)
                 .setClearBucket(true)
                 .createCameraBundle()
         )
         startActivityForResult(intent, 214)
 
-        var images = (activity as MainActivity).images
-
-        binding.carouselView.apply {
-            size = images.size
-            resource = R.layout.list_card
-            autoPlay = false
-            indicatorAnimationType = IndicatorAnimationType.THIN_WORM
-            carouselOffset = OffsetType.CENTER
-            setCarouselViewListener { view, position ->
-                // Example here is setting up a full image carousel
-                val imageView = view.findViewById<ImageView>(R.id.listImageView)
-                imageView.setImageBitmap(images[position])
-            }
-            // After you finish setting up, show the CarouselView
-            show()
-        }
     }
 
-    protected override fun onActivityResult(
+     override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
         @Nullable data: Intent?
@@ -68,7 +53,23 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         if (requestCode == 214) {
             if (resultCode == RESULT_OK) {
                 assert(data != null)
-                list = data!!.getStringArrayExtra("resultData")
+                var images = data!!.getStringArrayExtra("resultData")
+
+                binding.carouselView.apply {
+                    size = images?.size!!
+                    resource = R.layout.list_card
+                    autoPlay = false
+                    indicatorAnimationType = IndicatorAnimationType.THIN_WORM
+                    carouselOffset = OffsetType.CENTER
+                    setCarouselViewListener { view, position ->
+                        // Example here is setting up a full image carousel
+                        val imageView = view.findViewById<ImageView>(R.id.listImageView)
+                        val bitmap = BitmapFactory.decodeFile(images[position])
+                        imageView.setImageBitmap(bitmap)
+                    }
+                    // After you finish setting up, show the CarouselView
+                    show()
+                }
             }
         }
     }
--- app/src/main/java/com/nativecoders/scanmate/MainActivity.kt
+++ app/src/main/java/com/nativecoders/scanmate/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.nativecoders.scanmate
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import com.camerakit.CameraKitView
 import com.nativecoders.scanmate.databinding.ActivityMainBinding
-import com.nativecoders.scanmate.databinding.FragmentCameraBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
     lateinit var binding: ActivityMainBinding
 
+    lateinit var images: ArrayList<Bitmap>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        images = ArrayList()
 
         navController = findNavController(R.id.nav_host_fragment)
     }
