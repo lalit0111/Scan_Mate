53ad4cec66c89b03e6cad3a91800f29ee0a5530d
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2021-05-02 17:36:41 +0530

    conflict resolved

--- app/src/main/java/com/nativecoders/scanmate/ListFragment.kt
+++ app/src/main/java/com/nativecoders/scanmate/ListFragment.kt
@@ -1,12 +1,16 @@
 package com.nativecoders.scanmate
 
+import `in`.balakrishnan.easycam.CameraBundleBuilder
+import `in`.balakrishnan.easycam.CameraControllerActivity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import com.jama.carouselview.CarouselView
+import androidx.annotation.Nullable
+import androidx.navigation.fragment.findNavController
 import com.jama.carouselview.enums.IndicatorAnimationType
 import com.jama.carouselview.enums.OffsetType
 import com.nativecoders.scanmate.databinding.FragmentListBinding
@@ -40,7 +44,7 @@ class ListFragment : Fragment(R.layout.fragment_list) {
 
     }
 
-     override fun onActivityResult(
+    override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
         @Nullable data: Intent?
@@ -51,29 +55,30 @@ class ListFragment : Fragment(R.layout.fragment_list) {
                 assert(data != null)
                 var images = data!!.getStringArrayExtra("resultData")
 
-        binding.carouselView.apply {
-            size = images.size
-            resource = R.layout.list_card
-            autoPlay = false
-            indicatorAnimationType = IndicatorAnimationType.THIN_WORM
-            carouselOffset = OffsetType.CENTER
-            setCarouselViewListener { view, position ->
-                // Example here is setting up a full image carousel
-                val imageView = view.findViewById<ImageView>(R.id.listImageView)
-                imageView.setImageBitmap(images[position])
-            }
-            // After you finish setting up, show the CarouselView
-            show()
-        }
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.reorderImage -> {
-                    findNavController().navigate(R.id.action_listFragment_to_reorderFragment)
-                    true
+                binding.carouselView.apply {
+                    size = images?.size!!
+                    resource = R.layout.list_card
+                    autoPlay = false
+                    indicatorAnimationType = IndicatorAnimationType.THIN_WORM
+                    carouselOffset = OffsetType.CENTER
+                    setCarouselViewListener { view, position ->
+                        val imageView = view.findViewById<ImageView>(R.id.listImageView)
+                        val bitmap = BitmapFactory.decodeFile(images[position])
+                        imageView.setImageBitmap(bitmap)
+                    }
+                    show()
                 }
-                else -> false
+                binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+                    when (item.itemId) {
+                        R.id.reorderImage -> {
+                            findNavController().navigate(R.id.action_listFragment_to_reorderFragment)
+                            true
+                        }
+                        else -> false
+                    }
+                }
             }
+
         }
     }
-
 }
