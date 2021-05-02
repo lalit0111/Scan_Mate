5f63e8423190b6c0e0862e3e6c3ee7de379a1747
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2021-05-02 18:03:07 +0530

    item touch helper added

--- app/src/main/java/com/nativecoders/scanmate/ReorderAdapter.kt
+++ app/src/main/java/com/nativecoders/scanmate/ReorderAdapter.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.nativecoders.scanmate.databinding.ReorderListCardBinding
 
-class ReorderAdapter(var list:ArrayList<Int>) : RecyclerView.Adapter<ReorderAdapter.ViewHolder>() {
+class ReorderAdapter(var list:ArrayList<Int> ) : RecyclerView.Adapter<ReorderAdapter.ViewHolder>() {
     class ViewHolder(var binding: ReorderListCardBinding) : RecyclerView.ViewHolder(binding.root)
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder= ViewHolder(
         ReorderListCardBinding
@@ -17,9 +19,23 @@ class ReorderAdapter(var list:ArrayList<Int>) : RecyclerView.Adapter<ReorderAdap
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
        holder.apply {
-           binding.listImageView.setImageResource(list[position])
+           binding.listImageView.apply {
+               setImageResource(list[position])
+
+           }
+
        }
     }
 
     override fun getItemCount(): Int = list.size
+
+    fun moveItem(from: Int, to: Int) {
+        val fromEmoji = list[from]
+        list.removeAt(from)
+        if (to < from) {
+            list.add(to, fromEmoji)
+        } else {
+            list.add(to - 1, fromEmoji)
+        }
+    }
 }
--- app/src/main/java/com/nativecoders/scanmate/ReorderFragment.kt
+++ app/src/main/java/com/nativecoders/scanmate/ReorderFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.nativecoders.scanmate.databinding.FragmentReorderBinding
 import com.nativecoders.scanmate.databinding.ReorderListCardBinding
@@ -13,6 +16,8 @@ import com.nativecoders.scanmate.databinding.ReorderListCardBinding
 class ReorderFragment : Fragment(R.layout.fragment_reorder) {
 
     lateinit var binding: FragmentReorderBinding
+    lateinit var reorderAdapter: ReorderAdapter
+    lateinit var itemTouchHelper: ItemTouchHelper
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentReorderBinding.bind(view)
@@ -20,11 +25,56 @@ class ReorderFragment : Fragment(R.layout.fragment_reorder) {
         images.add(R.drawable.img1)
         images.add(R.drawable.img2)
         images.add(R.drawable.img3)
-        var adapter0= ReorderAdapter(images)
+       reorderAdapter = ReorderAdapter(images)
         binding.recView.apply {
-            adapter = adapter0
+            adapter = reorderAdapter
             layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+            itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+            itemTouchHelper.attachToRecyclerView(this)
         }
+    }
+
+    val simpleItemTouchCallback =
+        object : ItemTouchHelper.SimpleCallback(UP or
+                DOWN or
+                START or
+                END, 0) {
+
+            override fun onMove(recyclerView: RecyclerView,
+                                viewHolder: RecyclerView.ViewHolder,
+                                target: RecyclerView.ViewHolder): Boolean {
+
+                val adapter = recyclerView.adapter as ReorderAdapter
+                val from = viewHolder.adapterPosition
+                val to = target.adapterPosition
+                // 2. Update the backing model. Custom implementation in
+                //    MainRecyclerViewAdapter. You need to implement
+                //    reordering of the backing model inside the method.
+                adapter.moveItem(from, to)
+                // 3. Tell adapter to render the model update.
+                adapter.notifyItemMoved(from, to)
+
+                return true
+            }
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder,
+                                  direction: Int) { }
+            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+                super.onSelectedChanged(viewHolder, actionState)
+
+                if (actionState == ACTION_STATE_DRAG) {
+                    viewHolder?.itemView?.alpha = 0.5f
+                }
+            }
+
+            override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+                super.clearView(recyclerView, viewHolder)
+
+                viewHolder.itemView.alpha = 1.0f
+            }
+        }
+
+    fun startDragging(viewHolder: RecyclerView.ViewHolder) {
+        itemTouchHelper.startDrag(viewHolder)
     }
 
 }
